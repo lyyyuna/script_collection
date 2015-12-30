c800c9d5f575a8b9417e14ea7362bb46d4120228
Li Yiyang <lyyyuna@gmail.com>
2015-12-30 10:26:16 +0800

    add auto ping service to baidu & bing

--- baidu_url_auto_submit/1.py
+++ baidu_url_auto_submit/1.py
@@ -27,7 +27,58 @@ def check_if_exist(url):
 
     return False
 
+def auto_ping(url):
+    sitemap_url = 'http://www.lyyyuna.com/sitemap.xml'
+    bing_ping = 'http://www.bing.com/webmaster/ping.aspx?siteMap=' + sitemap_url
+
+    get = urllib2.urlopen(bing_ping)
+    result = get.read()
+    print result
+    print
+
+
+    ## import xml.etree.ElementTree as ET
+
+    root = ET.Element("methodCall")
+    methodname = ET.SubElement(root, "methodName").text = 'weblogUpdates.extendedPing'
+    params = ET.SubElement(root, "params")
+
+
+    param = ET.SubElement(params, "param")
+    value = ET.SubElement(param, 'value')
+    string = ET.SubElement(value, 'string').text = u'lyyyuna 的小花园'
+
+    param = ET.SubElement(params, "param")
+    value = ET.SubElement(param, 'value')
+    string = ET.SubElement(value, 'string').text = u'http://www.lyyyuna.com/'
+
+    param = ET.SubElement(params, "param")
+    value = ET.SubElement(param, 'value')
+    string = ET.SubElement(value, 'string').text = url
+
+    param = ET.SubElement(params, "param")
+    value = ET.SubElement(param, 'value')
+    string = ET.SubElement(value, 'string').text = u'http://www.lyyyuna.com/atom.xml'
+
+    # tree = ET.ElementTree(root)
+    xmlstr = ET.tostring(root, encoding='utf8', method='xml')
+    # print xmlstr
+    # print
+
+    baidu_pingRPC = 'http://ping.baidu.com/ping/RPC2'
+    req = urllib2.Request(baidu_pingRPC, xmlstr)
+    response = urllib2.urlopen(req)
+    the_page = response.read()
+
+    print the_page
+    print
+
+
+
 values = ''
+new_blog_flag = False
+new_blog_url = ''
+
 loc_string = '{http://www.sitemaps.org/schemas/sitemap/0.9}loc'
 for url in root:
     for attr in url:
@@ -37,9 +88,16 @@ for url in root:
                 cur.execute("insert into urls (url) values ('" + attr.text + "')")
                 values = values + attr.text + '\n'
 
+                new_blog_flag = True
+                new_blog_url = attr.text;
+
 cx.commit()
 
 print values
+
+if new_blog_flag:
+    auto_ping(new_blog_url)
+
 
 baidu_zhanzhang_url = 'http://data.zz.baidu.com/urls?site=www.lyyyuna.com&token=XXXXXXXXXXXXXXXXXX&type=original'
 # values = 'http://www.lyyyuna.com/2015/12/28/robotframework-quickstartguide/'
