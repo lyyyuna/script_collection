928417b94d8b8f883a1579daa46f065780c1309f
Li Yiyang <lyyyuna@gmail.com>
2016-03-18 17:10:07 +0800

    gray hat

---
+++ gray_hat_python/my_debugger_definee.py
@@ -0,0 +1,42 @@
+from ctypes import *
+
+# map microsoft types to ctypes
+WORD = c_ushort
+DWORD = c_ulong
+LPBYTE = POINTER(c_ubyte)
+LPTSTR = POINTER(c_char)
+HANDLE = c_void_p
+
+# contants
+DEBUG_PROCESS = 0x00000001
+CREATE_NEW_CONSOLE = 0x00000010
+
+# structure for CreateProcessA() function
+class STARTUPINFO(Structure):
+    _fields_ = [
+        ("cb", DWORD),
+        ("lpReserved", LPTSTR),
+        ("lpDesktop", LPTSTR),
+        ("lpTitle", LPTSTR),
+        ("dwX", DWORD),
+        ("dwY", DWORD),
+        ("dwXSize", DWORD),
+        ("dwYSize", DWORD),
+        ("dwXCountChars", DWORD),
+        ("dwYCountChars", DWORD),
+        ("dwFillAttribute",DWORD),
+        ("dwFlags", DWORD),
+        ("wShowWindow", WORD),
+        ("cbReserved2", WORD),
+        ("lpReserved2", LPBYTE),
+        ("hStdInput", HANDLE),
+        ("hStdOutput", HANDLE),
+        ("hStdError", HANDLE),
+        ]
+
+class PROCESS_INFORMATION(Structure):
+    _fields_ = [
+        ("hProcess", HANDLE),
+        ("hThread", HANDLE),
+        ("dwProcessId", DWORD),
+    ]
---
+++ gray_hat_python/my_test.py
@@ -0,0 +1,4 @@
+import my_debugger
+
+debugger = my_debugger.debugger()
+debugger.load('C:\\WINDOWS\\system32\\calc.exe')
