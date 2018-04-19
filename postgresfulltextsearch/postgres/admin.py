1c56abbad0c45172114570181badebac94bf8fb9
lyyyuna <lyyyuna@gmail.com>
2018-04-19 17:59:46 +0800

    add model to admin

--- postgresfulltextsearch/postgres/admin.py
+++ postgresfulltextsearch/postgres/admin.py
@@ -2,5 +2,23 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from .models import Author
+from .models import Blog
+from .models import Entry
 
-# Register your models here.
+
+class AuthorAdmin(admin.ModelAdmin):
+    pass
+
+
+class BlogAdmin(admin.ModelAdmin):
+    pass
+
+
+class EntryAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Blog, BlogAdmin)
+admin.site.register(Entry, EntryAdmin)
