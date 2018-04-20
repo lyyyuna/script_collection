d67f1750c5d8d5e45361fa08fdbc04b8a1dd8292
lyyyuna <lyyyuna@gmail.com>
2018-04-20 16:57:05 +0800

    new

---
+++ full_text_search/blogapp/models.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+class Blog(models.Model):
+    name = models.CharField(max_length=100, default='lyy')
+    content = models.TextField(default='This is a excellent blog.')
+
+    def __str__(self):
+        return name
