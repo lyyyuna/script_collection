0f249bb2b80178f8c87e6c13aa00b77d8ab93d51
lyyyuna <lyyyuna@gmail.com>
2018-04-28 11:14:37 +0800

    update

--- orm_draft/subnew.py
+++ orm_draft/subnew.py
@@ -1,12 +1,11 @@
-class Model(dict):
+class ModelType(type):
     def __new__(cls, name, bases, attrs):
         tablename = attrs.get('__table__', None) or name
-        print ('Get table name', tablename)
+        print 'Get table name', tablename
         return type.__new__(cls, name, bases, attrs)
 
+class Model(dict):
+    __metaclass__ = ModelType
 
 class User(Model):
     __table__ = 'User_table'
-
-
-u1 = User(3, 4, 5)
