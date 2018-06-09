b157febe402a6b2e456061dde11ee1119c2f3ff0
lyyyuna <lyyyuna@gmail.com>
2018-06-09 23:04:19 +0800

    async server

---
+++ asyncfib/aserver.py
@@ -0,0 +1,31 @@
+from socket import *
+from concurrent.futures import ProcessPoolExecutor as Pool 
+from fib import fib
+from threading import Thread
+
+pool = Pool(4)
+
+def fib_server(address):
+    sock = socket(AF_INET, SOCK_STREAM)
+    sock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+    sock.bind(address)
+    sock.listen(5)
+    while True:
+        client, addr = sock.accept()
+        print("Connection", addr)
+        Thread(target=fib_handler, args=(client,), daemon=True).start()
+    
+
+def fib_handler(client):
+    while True:
+        req = client.recv(100)
+        if not req:
+            break
+        n = int(req)
+        future = pool.submit(fib, n)
+        result = future.result()
+        resp = str(result).encode('ascii') + b'\n'
+        client.send(resp)
+    print('Closed')
+
+fib_server(('', 25000))
